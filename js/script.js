d8aaad22fd15a6393a1062c1c9eb4e6b45a4343d
mimaxm <mimaxm@yandex.ru>
2021-05-25 11:40:13 +0300

    виджет соц сетей и кнопка вверх

--- js/script.js
+++ js/script.js
@@ -71,4 +71,46 @@ $(document).ready(function() {
           $('.header__burger, .menu').removeClass('active');
           $('.body').removeClass('lock');
       });
+
+      $(document).ready(function() {
+        // Show or hide the sticky footer button
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+          } else {
+            $('.go-top').fadeOut(200);
+          }
+        });
+        
+        // Animate the scroll to top
+        $('.go-top').click(function(event) {
+          event.preventDefault();
+          
+          $('html, body').animate({scrollTop: 0}, 300);
+        })
+      });
     });
+
+    var arrgetbtn=[];
+      arrgetbtn.push({"title":"Viber","icon":"fwidgethelp-viber","link":"viber://chat?number=%2B375333374006","target":"_blank","color":"#FFFFFF","background":"#8456A7"});
+      arrgetbtn.push({"title":"VKontakte","icon":"fwidgethelp-vk","link":"https://vk.com/im?sel=-100368303","target":"_blank","color":"#FFFFFF","background":"#507299"});
+      arrgetbtn.push({"title":"Telegram","icon":"fwidgethelp-telegram_v2","link":"https://tttttt.me/avtospeccentr","target":"_blank","color":"#FFFFFF","background":"#27A5E7"});
+        var WidGetButtonOptions = {
+          id:"544f5d178d4fb631ff496a68755a4eb3",
+          iconopen:"fwidgethelp-commenting-o",
+          maintitle:"",
+          mainbackground:"#46dee8",
+          maincolor:"#f3f3f3",
+          pulse:"widgethelp_pulse",
+          fasize:"2",
+          direction:"top",
+          position:"rightbottom",
+          arrbtn: arrgetbtn
+      };
+      (function() {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.async = true;
+        script.src = "https://getbtn.com/widget/index.php?id="+WidGetButtonOptions.id;
+        document.getElementsByTagName("head")[0].appendChild(script);
+      })();
